a6ee6f489c7834199b066deaa0cd33532b9d2eef
George Atkinson <georgeatkinsonwd@gmail.com>
2022-11-15 14:41:01 +0000

    crud requests

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -12,6 +12,16 @@ module.exports = {
             }
         })
     },
+
+    getWeightClass: async (req,res)=>{
+        const fighter = await FighterModel.find({weightClass: req.params.weightClass})
+        try {
+            res.json(fighter)
+        } catch (err) {
+            console.log(err)
+        }
+    },
+
     getFighter: async (req,res)=>{
         const fighter = await FighterModel.findById({_id: req.params.id})
         try{
@@ -21,6 +31,9 @@ module.exports = {
         console.log(err)
     }
     },
+
+   
+
     createFighter: async (req,res)=>{
         const fighter = req.body
         const newFighter = new FighterModel(fighter)
@@ -37,6 +50,38 @@ module.exports = {
             const update = await FighterModel.findOne({_id:req.params.id})
             res.json(update)
 
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
+    changeAge: async (req,res)=>{
+        const fighter = await FighterModel.findByIdAndUpdate({_id:req.params.id},req.body)
+        
+        try {
+            const update = await FighterModel.findOne({_id:req.params.id})
+            res.json(update)
+
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    deleteFighter: async(req,res)=>{
+        
+        try {
+            await FighterModel.findByIdAndDelete({_id:req.params.id},req.body)
+            console.log('Fighter Deleted')
+            res.json('Fighter Deleted')
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    updateRecord: async(req,res)=> {
+        const fighter = await FighterModel.findByIdAndUpdate({_id:req.params.id},req.body)
+        try {
+            const update = await FighterModel.findOne({_id:req.params.id})
+            console.log('record updated')
+            res.json(update)
         } catch (error) {
             console.log(error)
         }
--- routes/home.js
+++ routes/home.js
@@ -6,11 +6,20 @@ const homeController = require('../controllers/homeController')
 
 router.get('/getFighters', homeController.getFighters)
 
+router.get('/getFighters/:weightClass',homeController.getWeightClass)
+
 router.get('/getFighter/:id',homeController.getFighter)
 
 router.post('/createFighter', homeController.createFighter)
 
 router.put('/changeRank/:id', homeController.changeRank)
+
+router.put('/changeAge/:id', homeController.changeAge)
+
+router.put('/updateRecord/:id', homeController.updateRecord)
+
+router.delete('/deleteFighter/:id', homeController.deleteFighter)
+
 
 module.exports = router
 
